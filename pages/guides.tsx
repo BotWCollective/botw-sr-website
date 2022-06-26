0d12b7659c699ebf61995775ef91e2dd223658ab
Z2W00 <ethan.courau@icloud.com>
2022-06-26 20:17:51 +0100

    created banner

--- pages/guides.tsx
+++ pages/guides.tsx
@@ -1,13 +1,15 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import Banner from '../components/Banner';
 import Button from '../components/ButtonPrimary';
+import Card from '../components/Card'
 
 const GuidesPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
+    <Banner>Guides</Banner>
     <h1>Hehehaha</h1>
     <Button>eat my</Button>
   </Layout>
 );
   
 export default GuidesPage;
-//test
