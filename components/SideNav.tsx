eb76843ef3fe4e289adb3598236862c0985a3eae
Z2W00 <ethan.courau@icloud.com>
2022-07-03 22:24:54 +0100

    a

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
+import React, { ReactPropTypes } from 'react';
 import styles from './SideNav.module.scss'
 
 type Props = {
-  list1?: string
-  list2?: string
-  list3?: string
-  list4?: string 
+  list?: string 
+  link?: string
 }
-//hardcoding the number of elements in the list should be fine because this module is only be used once (in the guides page)
+const ListArray = ({list}: Props) => [list]
+const LinkArray = ({link}: Props) => [link]
 
-const NavigationAnchor = ({list1,list2,list3,list4,}: Props) => (
+const NavigationAnchor = ({list}: Props) => (
   <div className= {styles.SideNav}>
     <ul>
-      <li><a href='#'>{list1}</a></li>
-      <li><a href='#'>{list2}</a></li>
-      <li><a href='#'>{list3}</a></li>
-      <li><a href='#'>{list4}</a></li>
+      <li><a>{ListArray}</a></li>
     </ul>
   </div>
 )
--- pages/resources.tsx
+++ pages/resources.tsx
@@ -2,12 +2,12 @@ import Layout from '../components/Layout'
 import Banner from '../components/Banner';
 import Button from '../components/ButtonPrimary';
 import Card from '../components/Card'
-import NavigationAnchor from '../components/SideNav';
+import SideNav from '../components/SideNav';
 
 const GuidesPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <Banner>Resources</Banner>
-    <NavigationAnchor list1='General tips' list2='Game mechanics' list3='Category specific resources' list4='Glitches'></NavigationAnchor>
+    <SideNav list = 'dsa'></SideNav>
     <h1>Hehehaha</h1>
     <Button>eat my</Button>
     <Card heading='BLSS' link='View full guide'>Blss is a staple of Breath of the wild speedrunning allowing you to traverse large distances almost instantly</Card>
