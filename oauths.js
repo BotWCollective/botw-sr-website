8d885e8b5d4666c57fe2dd98028bbb1974effc1d
savage13 <savage13@gmail.com>
2022-07-06 04:51:11 -0400

    Delete oauths.js

--- oauths.js
+++
@@ -1,9 +0,0 @@
-let config = {
-    github: {
-        clientID: '8c3a43c1253f525bf8ee',
-        clientSecret: '6794394b414a3029971579f04b8faf69c92ca54a',
-        callbackURL: 'http://127.0.0.1:3000/api/auth/callback/github',
-    }
-};
-
-module.exports = config;
